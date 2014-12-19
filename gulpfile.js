494f546074cfa3241ed3d84c54b948c90ade5464
Steve Good <steve@stevegood.org>
2014-12-18 21:40:28 -0600

    Fleshing out the application using bootstrap and node-webkit

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,56 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefix = require('gulp-autoprefixer'),
+    notify = require('gulp-notify'),
+    bower = require('gulp-bower');
 
-gulp.task('default', function() {
-    // place code for your default task here
+var config = {
+    sassPath: './resources/sass',
+    bowerDir: './bower_components'
+};
+
+gulp.task('bower', function() {
+    return bower()
+        .pipe(gulp.dest(config.bowerDir));
 });
+
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+        .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('css', function() {
+    return gulp.src(config.sassPath + '/style.scss')
+        .pipe(
+            sass({
+                style: 'compressed',
+                "sourcemap=none": true,
+                loadPath: [
+                    './resources/sass',
+                    config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+                    config.bowerDir + '/fontawesome/scss',
+                ]
+             })
+            .on('error', notify.onError(function(error) {
+                return 'Error: ' + error.message;
+            }))
+        )
+        .pipe(autoprefix('last 2 version'))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+            config.bowerDir + '/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+            config.bowerDir + '/jquery/dist/jquery.min.*'
+        ])
+        .pipe(gulp.dest('./public/js'));
+});
+
+// rerun the task when a file changes
+gulp.task('watch', function() {
+    gulp.watch(config.sassPath + '/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['bower', 'icons', 'css', 'js']);
+
